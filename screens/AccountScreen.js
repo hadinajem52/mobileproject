ae80fd244f98a32db95ea144ddd186f5d232bde4
hadinajem52 <132664445+hadinajem52@users.noreply.github.com>
2025-11-17 22:16:43 +0200

    ..

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -298,7 +298,7 @@ const styles = StyleSheet.create({
   },
   modalOverlay: {
     flex: 1,
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    backgroundColor: 'transparent',
     justifyContent: 'flex-end',
   },
   modalOverlayTouch: {
